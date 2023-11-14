a84c3afc7b167bdf8c4e75b31d22d463b6593bec
Bruno Moreira <brunomcmoreira99@gmail.com>
2023-11-14 22:18:35 +0000

    Add console utils

---
+++ console_utils.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include "console_utils.h"
+
+void enableVirtualTerminalProcessing(void) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hConsole == INVALID_HANDLE_VALUE) return;
+    DWORD dwMode = 0;
+    if (GetConsoleMode(hConsole, &dwMode)) SetConsoleMode(hConsole, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+}
+
+void setForegroundColor(const int color) {
+    const int r = color >> 16 & 0xFF;
+    const int g = color >> 8 & 0xFF;
+    const int b = color & 0xFF;
+    printf("\x1B[38;2;%d;%d;%dm", r, g, b);
+}
+
+void setBackgroundColor(const int color) {
+    const int r = color >> 16 & 0xFF;
+    const int g = color >> 8 & 0xFF;
+    const int b = color & 0xFF;
+    printf("\x1B[48;2;%d;%d;%dm", r, g, b);
+}
+
+int getCursorPosition(void) {
+    int cursorTop, cursorLeft;
+    // Query the cursor position.
+    printf("\x1B[6n");
+    char cursorPosReport[100] = "";
+    int i = 0, ch;
+    while ((ch = _getch()) != 'R') {
+        if (ch == EOF) break;
+        if (isprint(ch) && i + 1 < 100) {
+            cursorPosReport[i++] = (char)ch;
+        }
+    }
+    cursorPosReport[i] = '\0';
+    if (sscanf(cursorPosReport, "[%d;%d", &cursorTop, &cursorLeft) != 2) return 0;
+    return cursorTop << 16 | cursorLeft;
+}
+
+int getConsoleDimensions(void) {
+    // This is a little bit of an hack, but good enough.
+    const int currentCursorPosition = getCursorPosition();
+    // Move cursor the bottom right corner.
+    printf("\x1B[9999;9999H");
+    const int result = getCursorPosition();
+    // Move cursor back to its original position.
+    printf("\x1B[%d;%dH", currentCursorPosition >> 16 & 0xFFFF, currentCursorPosition & 0xFFFF);
+    return result;
+}
+
+int getConsoleWidth(void) {
+    return getConsoleDimensions() & 0xFFFF;
+}
+
+int getConsoleHeight(void) {
+    return getConsoleDimensions() >> 16 & 0xFFFF;
+}
---
+++ console_utils.h
@@ -0,0 +1,47 @@
+#ifndef CONSOLE_UTILS_H
+#define CONSOLE_UTILS_H
+
+typedef enum {
+    WHITE = 0xFFFFFF,
+    RED = 231 << 16 | 72 << 8 | 86, // 0xE74856
+    // TODO: Add more colors.
+} Color;
+
+void enableVirtualTerminalProcessing(void);
+int getCursorPosition(void);
+int getConsoleDimensions(void);
+int getConsoleWidth(void);
+int getConsoleHeight(void);
+
+void setForegroundColor(int color);
+void setBackgroundColor(int color);
+
+static inline void clearConsole(void) {
+    printf("\x1B[H\x1B[2J\x1B[3J");
+}
+
+static inline void resetForegroundColor(void) {
+    printf("\x1B[39m");
+}
+
+static inline void resetBackgroundColor(void) {
+    printf("\x1B[49m");
+}
+
+static inline void resetTextDecoration(void) {
+    printf("\x1B[0m");
+}
+
+static inline void showCursor(void) {
+    printf("\x1B[?25h");
+}
+
+static inline void hideCursor(void) {
+    printf("\x1B[?25l");
+}
+
+static inline void resetCursorPosition(void) {
+    printf("\x1B[H");
+}
+
+#endif // CONSOLE_UTILS_H
